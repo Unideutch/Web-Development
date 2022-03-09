01c994a97f1f6830c8ac9dbfd88723828bb5d446
Unideutch <volkov.semen03@mail.ru>
2022-03-09 16:35:20 +0300

    Update 32.php

--- lab3/32/32.php
+++ lab3/32/32.php
@@ -5,29 +5,21 @@ $condition = 1;
 if(!ctype_alpha($string[0]))
 {
     $condition = 0;
-    $message = 1;
-}
-for($i=1; $i < strlen($string); $i++)
-{
-    if(!ctype_alpha($string[$i]) && !is_numeric($string[$i]))
-    {
-        $condition = 0;
-        $message = 2;
-    }
-}
-if ($condition == 1)
-{
-    echo 'Yes';
+    echo "No \nИдентефикатор не должен начинаться с цифры";
 }
 else
 {
-    echo 'No';
+    for($i=1; $i < strlen($string); $i++)
+    {
+        if(!ctype_alpha($string[$i]) && !is_numeric($string[$i]))
+        {
+            $condition = 0;
+            echo "No \n идентефикатор н е должен содержать спец. знаков";
+            break;
+        }
+    }
 }
-if ($message == 1)
+if($condition == 1)
 {
-    echo "\nИдентефикатор не должен начинаться с цифры";
-}
-if ($message == 2)
-{
-    echo "\nИдентефикатор не должен содержать специальных символов";
+    echo "Yes";
 }
